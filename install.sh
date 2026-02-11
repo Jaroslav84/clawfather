5af937c3dab0d76526ed2225badcc21846e788fe
Yaro <admin@badcia.com>
2026-02-11 07:47:10 +0100

    kickstarting curl -fsSL install.sh | bash

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-# Bootstrap: downloads clawfather and runs the real installer.
+# Bootstrap: downloads clawfather into current directory and runs the real installer.
 # Usage: curl -fsSL https://raw.githubusercontent.com/Jaroslav84/clawfather/master/install.sh | bash
 
 set -e
 REPO="https://github.com/Jaroslav84/clawfather/archive/refs/heads/master.tar.gz"
-TMP=$(mktemp -d)
-trap "rm -rf $TMP" EXIT
-curl -fsSL "$REPO" | tar xz -C "$TMP"
-cd "$TMP/clawfather-master"
+DEST="$(pwd)"
+curl -fsSL "$REPO" | tar xz -C "$DEST"
+cd "$DEST/clawfather-master"
 chmod +x src/install_clawfather.sh
 exec bash src/install_clawfather.sh "$@"
